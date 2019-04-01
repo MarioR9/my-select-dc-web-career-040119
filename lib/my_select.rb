f9401efc926598defff490563f3d36b768305124
mario rodrigueza <mariorodriguezan@gmail.com>
2019-04-01 00:53:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,17 +1,12 @@
 def my_select(collection)
  # your code here!
  i = 0
-
-  truthy_collection = []
-
+  t_collection = []
   while i < collection.length
-
     if yield(collection[i])
-
-      truthy_collection << collection[i]
-
+      t_collection << collection[i]
     end
     i += 1
   end
-  truthy_collection
+  t_collection
 end
