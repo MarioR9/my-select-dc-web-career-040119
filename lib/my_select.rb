9a875e084243baba96080ece5d3e9593f619fbec
mario rodrigueza <mariorodriguezan@gmail.com>
2019-04-01 00:49:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,7 +6,7 @@ def my_select(collection)
       new_collect = yield(collection[i])
       array << new_collect
         i+=1
-        return true
+        
      end
-       array
+       return true
 end
